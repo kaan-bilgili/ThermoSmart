892b94abcd70679b150d393865f67fea8b333627
Kaan Bilgili <kaan.bilgili@std.izmirekonomi.edu.tr>
2026-03-04 23:54:05 +0300

    Adjust layout, styles and add interactive widgets
    Tweak UI: reduce app title font, increase body text size, remove crossAxisAlignment.stretch, and adjust spacing (SizedBox). Resize network and asset images for better proportions. Add basic interactive controls (ElevatedButton, TextButton, Icons, IconButtons) with placeholder print callbacks and minor style changes to buttons and text.

--- flutterproject/lib/main.dart
+++ flutterproject/lib/main.dart
@@ -20,13 +20,12 @@ class MyWidget extends StatelessWidget {
           foregroundColor: const Color.fromARGB(255, 255, 255, 255),
           title: Text(
             'KONTES BIR CANAVAR TUM SINIF ONDAN KORKAR',
-            style: TextStyle(fontSize: 40.0, fontWeight: FontWeight(300)),
+            style: TextStyle(fontSize: 30.0, fontWeight: FontWeight(300)),
           ),
         ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               Text(
                 'GENETICS LOVER!',
@@ -35,21 +34,54 @@ class MyWidget extends StatelessWidget {
               SizedBox(height: 25),
               Text(
                 'I am a genetics lover and I am passionate about learning more about the field. I enjoy reading about the latest research and discoveries in genetics, and I am always eager to expand my knowledge. I find the complexity of genetics fascinating and I am excited to see how it continues to evolve in the future.',
-                style: TextStyle(fontSize: 16, fontFamily: 'Lora'),
+                style: TextStyle(fontSize: 21, fontFamily: 'Lora'),
               ),
               SizedBox(height: 32),
               Image.network(
                 "https://picsum.photos/400",
-                width: 100,
-                height: 100,
+                width: 200,
+                height: 200,
                 fit: BoxFit.contain,
               ),
-              SizedBox(height: 32),
+              SizedBox(height: 12),
               Image.asset(
                 "assets/images/photo.jpeg",
-                width: 400,
-                height: 300,
+                width: 300,
+                height: 200,
                 fit: BoxFit.cover,
+              ),
+              SizedBox(height: 12),
+              ElevatedButton(
+                onPressed: () {
+                  print("You have pressed"); //empty function for now
+                },
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.white),
+                child: Text(
+                  "Press me if you love kontes",
+                  style: TextStyle(fontSize: 23),
+                ),
+              ),
+
+              SizedBox(height: 3),
+              TextButton(
+                onPressed: () => print("U pressed the text"),
+
+                child: Text("Press me if you don't"),
+              ),
+              Icon(Icons.add_box_sharp, size: 22, color: Colors.red.shade400),
+              IconButton(
+                onPressed: () => print("You have pressed"),
+                icon: Icon(color: Colors.red, Icons.agriculture_rounded),
+              ),
+              IconButton(
+                onPressed: () => print("U pressed the text"),
+                icon: Icon(
+                  Icons.add_box_sharp,
+                  size: 22,
+                  opticalSize: 12,
+                  color: Colors.red.shade400,
+                  fill: 0.4,
+                ),
               ),
             ],
           ),
