68e4d3e73972adbf81e46b63d5fd7e29387e578c
Kaan Bilgili <kaan.bilgili@std.izmirekonomi.edu.tr>
2026-03-14 16:17:40 +0300

    Add MQTT telemetry and setpoint handling
    Replace the previous HTTPClient-based demo with MQTT publish/subscribe using PubSubClient. Introduces WiFiClient and PubSubClient, placeholders for WiFi/MQTT credentials, a reconnect routine, and a callback that updates a setpoint from topic "thermosmart/setpoint". Periodically publishes simulated temperature to "thermosmart/temperature" and removes the prior HTTP GET request and hardcoded WiFi credentials. Minor serial/logging tweaks and use of dtostrf for formatting temperature.

--- esp/SimpleWiFiServer/SimpleWiFiServer.ino
+++ esp/SimpleWiFiServer/SimpleWiFiServer.ino
@@ -1,41 +1,64 @@
- #include <WiFi.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
 
+const char* ssid = "";
+const char* password = "";
+const char* mqtt_server = "";
 
-#include <HTTPClient.h>
+WiFiClient espClient;
+PubSubClient client(espClient);
 
-const char* ssid = "COORDINAT_A409";
-const char* password = "banu_kaan_1";
+int setpoint = 22;
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  String message;
+  for (int i = 0; i < length; i++) {
+    message += (char)payload[i];
+  }
+  Serial.print("Setpoint received: ");
+  Serial.println(message);
+  setpoint = message.toInt();
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Connecting MQTT...");
+    if (client.connect("ESP32Thermostat")) {
+      Serial.println("connected");
+      client.subscribe("thermosmart/setpoint");
+    } else {
+      Serial.print("failed ");
+      Serial.println(client.state());
+      delay(2000);
+    }
+  }
+}
 
 void setup() {
-
   Serial.begin(115200);
-
   WiFi.begin(ssid, password);
-
+  Serial.print("Connecting WiFi");
   while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("WiFi baglaniyor...");
+    delay(500);
+    Serial.print(".");
   }
-
-  Serial.println("WiFi baglandi");
-
-  HTTPClient http;
-
-  http.begin("http://10.10.120.40:5000");
-
-  int httpCode = http.GET();
-
-  Serial.print("HTTP Response: ");
-  Serial.println(httpCode);
-
-  http.end();
+  Serial.println("OK");
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
 }
 
 void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
 
+  float temperature = random(20, 30);
+  char tempString[8];
+  dtostrf(temperature, 1, 2, tempString);
+  client.publish("thermosmart/temperature", tempString);
+  Serial.print("Temperature sent: ");
+  Serial.println(tempString);
+
+  delay(3000);
 }
-
-
-
-
-
