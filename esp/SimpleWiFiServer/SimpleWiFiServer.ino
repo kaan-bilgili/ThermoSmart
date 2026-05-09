06daadba2e1d6807e95b6bd993c52070fd966d81
Kaan Bilgili <kaan.bilgili@std.izmirekonomi.edu.tr>
2026-05-09 14:54:59 +0300

    dummy veri veren yeni kod

--- esp/SimpleWiFiServer/SimpleWiFiServer.ino
+++ esp/SimpleWiFiServer/SimpleWiFiServer.ino
@@ -1,28 +1,18 @@
 #include <WiFi.h>
-#include <WiFiManager.h>
 #include <PubSubClient.h>
-#include <DHT.h>
 
-// ===== PINLER =====
-#define DHTPIN 4
-#define DHTTYPE DHT11
-#define LED_PIN 5
+const char* ssid = "thermosmart";
+const char* password = "thermosmart";
 
-// ===== MQTT =====
 const char* mqtt_server = "192.168.1.100";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-// ===== DHT =====
-DHT dht(DHTPIN, DHTTYPE);
-
-// ===== TERMOSTAT =====
 int setpoint = 22;
 
-// ===== MQTT MESAJ GELİNCE =====
 void callback(char* topic, byte* payload, unsigned int length) {
-  String message;
+  String message = "";
 
   for (int i = 0; i < length; i++) {
     message += (char)payload[i];
@@ -34,80 +24,65 @@ void callback(char* topic, byte* payload, unsigned int length) {
   setpoint = message.toInt();
 }
 
-// ===== MQTT YENİDEN BAĞLAN =====
+void connectWiFi() {
+  Serial.println("WiFi baglaniyor...");
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println();
+  Serial.println("WiFi BAGLANDI");
+  Serial.print("ESP IP: ");
+  Serial.println(WiFi.localIP());
+}
+
 void reconnect() {
   while (!client.connected()) {
-    Serial.print("MQTT baglaniyor...");
+    Serial.println("MQTT connect deneniyor...");
 
-    if (client.connect("ESP32Thermostat")) {
-      Serial.println("baglandi");
+    if (client.connect("ESP32TEST")) {
+      Serial.println("MQTT CONNECT BASARILI");
+
       client.subscribe("thermosmart/setpoint");
+
     } else {
-      Serial.print("Hata kodu: ");
+      Serial.print("MQTT FAIL: ");
       Serial.println(client.state());
-      delay(2000);
+      delay(3000);
     }
   }
 }
 
-// ===== SETUP =====
 void setup() {
   Serial.begin(115200);
 
-  // WiFi bağlan (portal açılır)
-  WiFiManager wm;
-  wm.autoConnect("Thermostat-Setup");
+  connectWiFi();
 
-  Serial.println("WiFi OK");
-
-  // MQTT ayar
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
-
-  // Pinler
-  pinMode(LED_PIN, OUTPUT);
-
-  // DHT başlat
-  dht.begin();
 }
 
-// ===== LOOP =====
 void loop() {
-
-  // MQTT bağlı mı?
   if (!client.connected()) {
     reconnect();
   }
 
   client.loop();
 
-  // ===== SICAKLIK OKU =====
-  float temperature = dht.readTemperature();
+  float temperature = 25.0;
 
-  if (isnan(temperature)) {
-    Serial.println("DHT hata!");
-    delay(2000);
-    return;
-  }
-
-  // ===== MQTT GÖNDER =====
   char tempString[8];
   dtostrf(temperature, 1, 2, tempString);
 
   client.publish("thermosmart/temperature", tempString);
-
-  Serial.print("Temp: ");
+ nm
+  Serial.print("Dummy Temp: ");
   Serial.println(tempString);
-
-  // ===== TERMOSTAT =====
-  if (temperature < setpoint - 1) {
-    digitalWrite(LED_PIN, HIGH);
-    Serial.println("Heating ON");
-  }
-  else if (temperature > setpoint + 1) {
-    digitalWrite(LED_PIN, LOW);
-    Serial.println("Heating OFF");
-  }
 
   delay(3000);
 }
